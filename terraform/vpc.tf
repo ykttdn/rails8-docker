42b61a84d1970616287b51e6a29a81bc0c025d75
ykttdn <38461803+ykttdn@users.noreply.github.com>
2025-07-29 01:33:26 +0900

    Resource for subnet

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -6,3 +6,25 @@ resource "aws_vpc" "main" {
     Env  = "dev"
   }
 }
+
+resource "aws_subnet" "public" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = "ap-northeast-1a"
+
+  tags = {
+    Name = "public-subnet"
+    Env  = "dev"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-northeast-1c"
+
+  tags = {
+    Name = "private-subnet"
+    Env  = "dev"
+  }
+}
